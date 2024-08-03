63a1890b5fc6d42b8d6bc40216d73bc7062bec8b
lcamusm <lcamusm@uc.cl>
2024-08-03 18:43:21 -0400

    borrar start.sh

--- owen-wilson/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo "Starting server..."
-# Verifica la ruta actual y lista los archivos para depuración
-pwd
-ls -la
-# Verifica la versión de yarn
-yarn --version
-# Inicia el servidor
-yarn dev
